85792e2f7e6c43e08e109a5103e634537dea1eea
Benroyz <30813249+Benroyz@users.noreply.github.com>
2019-03-06 18:03:53 -0500

    Create mutechannel.py

---
+++ mutechannel/mutechannel.py
@@ -0,0 +1,28 @@
+import asyncio
+import discord
+from redbot.core import commands, checks
+
+class channelmute:
+    """Channel mute"""
+
+    def __init__(self, bot):
+        self.bot = bot
+    @checks.admin_or_permissions(manage_channels=True)
+    @commands.command(pass_context = True)
+    async def mutechannel(self, ctx):
+        perms = discord.PermissionOverwrite()
+        perms.send_messages = False
+        await self.bot.edit_channel_permissions(ctx.message.channel, ctx.message.server.default_role ,perms)
+        todelete = await self.bot.say("Done :+1:")
+        await asyncio.sleep(5)
+        await self.bot.delete_message(todelete)
+
+    @checks.admin_or_permissions(manage_channels=True)
+    @commands.command(pass_context = True)
+    async def unmutechannel(self, ctx):
+        perms = discord.PermissionOverwrite()
+        perms.send_messages = True
+        await self.bot.edit_channel_permissions(ctx.message.channel, ctx.message.server.default_role ,perms)
+        todelete = await self.bot.say("Done :+1:")
+        await asyncio.sleep(5)
+        await self.bot.delete_message(todelete)
