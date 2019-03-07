3d837923f123ba09db71ca63f9a6353197168cde
Benroyz <30813249+Benroyz@users.noreply.github.com>
2019-03-06 20:32:53 -0500

    Update mutechannel.py

--- mutechannel/mutechannel.py
+++ mutechannel/mutechannel.py
@@ -2,7 +2,7 @@ import asyncio
 import discord
 from redbot.core import commands, checks
 
-class ChannelMute(commands.Cog):
+class MuteChannel(commands.Cog):
     """Channel mute"""
 
     @checks.admin_or_permissions(manage_channels=True)
