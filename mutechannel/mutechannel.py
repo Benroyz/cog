ff98739b1fdebc1b7f0b76edeb5ae0a477a35f4d
Benroyz <30813249+Benroyz@users.noreply.github.com>
2019-03-06 20:57:36 -0500

    test 1

--- mutechannel/mutechannel.py
+++ mutechannel/mutechannel.py
@@ -15,7 +15,7 @@ class mutechannel(commands.Cog):
     async def mutechannel(self, ctx):
         perms = discord.PermissionOverwrite()
         perms.send_messages = False
-        await self.bot.edit_channel_permissions(ctx.message.channel, ctx.message.server.default_role ,perms)
+        await self.bot.edit_channel_permissions(ctx.message.channel, ctx.message.server.default_role, perms)
         todelete = await self.bot.say("Done :+1:")
         await asyncio.sleep(5)
         await self.bot.delete_message(todelete)
@@ -25,7 +25,7 @@ class mutechannel(commands.Cog):
     async def unmutechannel(self, ctx):
         perms = discord.PermissionOverwrite()
         perms.send_messages = True
-        await self.bot.edit_channel_permissions(ctx.message.channel, ctx.message.server.default_role ,perms)
+        await self.bot.edit_channel_permissions(ctx.message.channel, ctx.message.server.default_role, perms)
         todelete = await self.bot.say("Done :+1:")
         await asyncio.sleep(5)
         await self.bot.delete_message(todelete)
