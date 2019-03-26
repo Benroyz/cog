5a918818b908ca3be8b6adaf8e09870c6d897327
Benroyz <30813249+Benroyz@users.noreply.github.com>
2019-03-26 12:33:05 -0400

    Update tttchangelogs.py

--- tttchangelogs/tttchangelogs.py
+++ tttchangelogs/tttchangelogs.py
@@ -121,10 +121,10 @@ class TTTChangelogs(commands.Cog):
             self.usercache.append(author.id)
                     
             dm = await author.send("Copy and paste this ```__**x changes**__"
-                                    "*x:*"
-                                    "  -"
-                                    "  -"
-                                    "  -``` for the formatting. ")
+                                    "*x:\n*"
+                                    "  -\n"
+                                    "  -\n"
+                                    "  -\n``` for the formatting. ")
         
             def check_message(m):
                 return m.channel == dm.channel and m.author == author
