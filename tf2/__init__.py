3e63eb0627ee1cd8d357a15c8b20807d6c04dd1f
Benroyz <30813249+Benroyz@users.noreply.github.com>
2019-07-26 14:23:14 -0400

    Update __init__.py

--- tf2/__init__.py
+++ tf2/__init__.py
@@ -1,1 +1,7 @@
+from .2fortsuggest import 2FortSuggest
+from .hightowersuggest import HighTowerSuggest
 
+
+def setup(bot):
+    bot.add_cog(2FortSuggest(bot))
+    bot.add_cog(HighTowerSuggest(bot))
